e8b82d430fbac90104148c9a3f418c6c2800fb0c
Melf <cqc@melf.de>
2025-02-04 18:27:52 +0000

    test invalid qir from files

--- tests/check_test.py
+++ tests/check_test.py
@@ -20,47 +20,23 @@ import pytest
 import quantinuum_qircheck as qc
 
 
-def test_check_qir_simpel_wrong() -> None:
-
-    qir = """; ModuleID = 'test_pytket_qir'
-source_filename = "test_pytket_qir"
-
-%Qubit = type opaque
-%Result = type opaque
-
-define void @main() #0 {
-entry:
-  call void @__quantum__Qqis__h__body(%Qubit* null)
-  ret void
-}
-
-declare i1 @__quantum__qis__read_result__body(%Result*)
-
-declare void @__quantum__rt__int_record_output(i64, i8*)
-
-declare void @__quantum__rt__result_record_output(%Result*, i8*)
-
-declare void @__quantum__qis__h__body(%Qubit*)
-
-attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="custom" "required_num_qubits"="3" "required_num_results"="3" }
-
-!llvm.module.flags = !{!0, !1, !2, !3}
-
-!0 = !{i32 1, !"qir_major_version", i32 1}
-!1 = !{i32 7, !"qir_minor_version", i32 0}
-!2 = !{i32 1, !"dynamic_qubit_management", i1 false}
-!3 = !{i32 1, !"dynamic_result_management", i1 false}"""
-
-    with pytest.raises(ValueError) as e:
-        qc.qircheck(qir)
-
-    assert "Qqis" in str(e)
-
-
 def test_check_qir_fileset() -> None:
-    for file in listdir("qir"):
+    for file in listdir("qir/valid"):
 
-        with open(f"qir/{file}") as f:
+        with open(f"qir/valid/{file}") as f:
             qir_str = f.read()
 
             qc.qircheck(qir_str)
+
+
+def test_check_qir_invalid_fileset() -> None:
+    for file in listdir("qir/invalid"):
+
+        with open(f"qir/invalid/{file}") as f:
+            qir_str = f.read()
+
+            with pytest.raises(ValueError) as e:
+                qc.qircheck(qir_str)
+
+            if file == "invalid_1.ll":
+                assert "Qqis" in str(e)
